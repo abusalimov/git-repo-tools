54064f701b1e5510739a9c9f5c906c198fe1cdc4
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2015-02-13 14:29:00 +0300

    tree-fix-whitespaces.sh: Do not touch indentation

--- tree-fix-whitespaces.sh
+++ tree-fix-whitespaces.sh
@@ -8,5 +8,4 @@
 # Replaces indentation spaces with tabs.
 #
 
-find . -type f -exec sed -ri $'s/[ \t\r]*$//' '{}' \; \
-	-name '*.[Sch]' -exec sed -ri $':a;s/^(\t*) {4}/\\1\t/;ta' '{}' \;
+find . -type f -exec sed -ri $'s/[ \t\r]*$//' '{}' \;
