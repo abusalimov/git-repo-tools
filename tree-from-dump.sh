5c960a737a85eed5ea29a169b85e363702800c68
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2015-03-11 03:11:21 +0300

    Add tree-from-dump.sh

---
+++ tree-from-dump.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# dump_dir=/tmp/git-dump
+# git filter-branch \
+#     --tree-filter "tree-from-dump.sh $dump_dir" \
+# --tag-name-filter cat -- --all
+#
+set -e
+
+DUMP_DIR="$1"
+
+if [[ -z $DUMP_DIR ]]; then
+	>&2 echo "Usage: $0 <dump-dir>"
+	exit 1
+fi
+
+find . \( -type f -o -type l \) -print0 | while read -d $'\0' file; do
+	file_dir="$DUMP_DIR/${file// /-}"
+	sha1=$(git hash-object --no-filters -- "$file")
+
+	if [ -f "$file_dir"/*-$sha1 ]; then
+		cp -T $(set -- "$file_dir"/*-$sha1; echo $1) > "$file"
+	else
+		rm "$file"
+	fi
+done
+find . -depth -type d -empty -exec rmdir '{}' \+
