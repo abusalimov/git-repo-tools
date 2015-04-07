43636349ff2b1fe1b8ba0b7d1ee0e8af8254de23
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2015-04-07 19:33:15 +0300

    tree-fix-whitespaces.sh: don't touch intentation, add NL at EOF

--- tree-fix-whitespaces.sh
+++ tree-fix-whitespaces.sh
@@ -5,7 +5,9 @@
 # --tag-name-filter cat -- --all
 #
 # Removes trailing whitespaces, converts CRLF -> LF.
-# Replaces indentation spaces with tabs.
+# Ensures a newline at the end of file.
 #
 
-find . -type f -exec sed -ri $'s/[ \t\r]*$//' '{}' \;
+# XXX Must filter-out binary files
+#   -exec sh -c 'file -b --mime-type "$0" | grep -wq -e text -e xml' \;
+find . -type f -exec sed -i -e 's/[ \t]*$//' -e 's/\r//g' -e '/.$/a\' '{}' \;
